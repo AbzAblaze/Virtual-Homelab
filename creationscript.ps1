6c72a67ee57ae68ff5e686b42f4128a20e68eb0c
Abdi Warsame <46764176+AbzAblaze@users.noreply.github.com>
2021-12-10 22:03:24 +0000

    Add files via upload

---
+++ creationscript.ps1
@@ -0,0 +1,18 @@
+﻿# ----- Variables----- #
+$USERPASSWORDS   = "Password123"
+$USERLIST = Get-Content .\namesfile.txt
+# ------------------------------------------------------ #
+
+$password = ConvertTo-SecureString $USERPASSWORDS -AsPlainText -Force
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
+
+foreach ($n in $USERLIST) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+    
+    New-AdUser -AccountPassword $password `-GivenName $first `-Surname $last `-DisplayName $username `
+               -Name $username `-EmployeeID $username `-PasswordNeverExpires $true `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `-Enabled $true
+}
